2a64a1f1d8e39d6c8550559e6d6d00173ff93e68
leejieuns2 <2jjingni@gmail.com>
2021-06-20 23:33:41 +0900

    [210620] add project settings - #3

---
+++ toyproject/src/test/java/jieun/refactoring/toyproject/ToyprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package jieun.refactoring.toyproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToyprojectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
