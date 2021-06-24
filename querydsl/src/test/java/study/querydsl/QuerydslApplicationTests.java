c3744018436a374eaed5288a3425fc623c5d66d1
leejieuns2 <2jjingni@gmail.com>
2021-06-24 14:49:16 +0900

    [210624] add Section 4 - #4

--- querydsl/src/test/java/study/querydsl/QuerydslApplicationTests.java
+++ querydsl/src/test/java/study/querydsl/QuerydslApplicationTests.java
@@ -1,10 +1,10 @@
 package study.querydsl;
 
 import com.querydsl.jpa.impl.JPAQueryFactory;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Commit;
 import study.querydsl.entity.Hello;
 import study.querydsl.entity.QHello;
 
@@ -15,6 +15,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 @Transactional
+@Commit
 class QuerydslApplicationTests {
 
     @Autowired
